b178fca52d3aad679072b3cf15c7cf6c57d5fbf6
David Shorthouse <davidpshorthouse@gmail.com>
2012-02-02 13:19:07 -0500

    Converted variables to objects for better validation

--- app/assets/javascripts/jquery.grabtag.js
+++ app/assets/javascripts/jquery.grabtag.js
@@ -16,7 +16,8 @@
 
   "use strict";
 
-  var eventName       = "mouseup." + gt,
+  var GT              = {},
+      eventName       = "mouseup." + gt,
       eventNameResize = "mouseup." + gtr,
       sample_styles   =  [
         { 'background-color' : '#8dd3c7' },
@@ -34,13 +35,13 @@
         { 'background-color' : '#bfbada' },
         { 'background-color' : '#80b1d3' },
         { 'background-color' : '#c8c8c8' }
-      ],
+      ];
 
-  get_style = function(obj) {
+  GT.get_style = function(obj) {
     return JSON.stringify(obj).replace(/[{}"]/g, "").replace(",", ";");
-  },
+  };
 
-  convert_markup = function(obj) {
+  GT.convert_markup = function(obj) {
     var snippet = "",
         result  = $(obj).clone(),
         tag     = "";
@@ -53,9 +54,9 @@
              .wrap('<' + tag + '>' + $(this).text() + '</' + tag + '>').remove();
     });
     return result.html();
-  },
+  };
 
-  clear_selections = function() {
+  GT.clear_selections = function() {
     var sel;
 
     if(document.selection && document.selection.empty){
@@ -66,13 +67,13 @@
       sel = document.getSelection();
     }
     if(sel && sel.removeAllRanges) { sel.removeAllRanges(); }
-  },
+  };
 
-  build_resizer = function(type) {
+  GT.build_resizer = function(type) {
     return '<span class="' + gt + '-resizer ' + gt + '-resizer-' + type + '"></span>';
-  },
+  };
 
-  get_selections = function() {
+  GT.get_selections = function() {
     var sel;
 
     if(window.getSelection) {
@@ -83,9 +84,9 @@
       sel = document.selection.createRange();
     }
     return sel;
-  },
+  };
 
-  build_selector = function(title, innerContent, style, selector) {
+  GT.build_selector = function(title, innerContent, style, selector) {
     var classes = gt + '-selector ' + gt + '-tag',
         output  = "";
 
@@ -93,79 +94,82 @@
     output = '<span class="' + classes + '" style="' + style + '" title="' + title + '" data-' + gt + '="' + title + '">' + innerContent + '</span>';
     if(selector) { output = '<li>' + output + '</li>'; }
     return output;
-  },
+  };
 
-  range_intersects = function(range, node) {
+  GT.range_intersects = function(range, node) {
     var nodeRange = document.createRange();
 
     try {
       nodeRange.selectNode(node);
-    } catch(e) {
+    } catch(error) {
       nodeRange.selectNodeContents(node);
     }
 
   return range.compareBoundaryPoints(Range.END_TO_START, nodeRange) === -1 &&
          range.compareBoundaryPoints(Range.START_TO_END, nodeRange) === 1;
-  },
+  };
 
-  range_intersects_tags = function(range, obj) {
-    var intersects = false;
+  GT.range_intersects_tags = function(range, obj) {
+    var self       = this,
+        intersects = false;
 
     $('.' + gt + '-tag', $(obj)).each(function() {
-      if(range_intersects(range, this)) {
+      if(self.range_intersects(range, this)) {
         intersects = true;
         return;
       }
     });
     return intersects;
-  },
+  };
 
-  range_intersects_tag = function(range, obj, tag) {
-    var intersects = false;
+  GT.range_intersects_tag = function(range, obj, tag) {
+    var self       = this,
+        intersects = false;
 
     $('.' + gt + '-tag', $(obj)).not($(tag)).each(function() {
-      if(range_intersects(range, this)) {
+      if(self.range_intersects(range, this)) {
         intersects = true;
         return;
       }
     });
     return intersects;
-  },
+  };
 
-  add_resizers = function(obj, settings, newNode) {
-    var resizer_w = build_resizer('w'),
-        resizer_e = build_resizer('e');
+  GT.add_resizers = function(obj, settings, newNode) {
+    var self      = this,
+        resizer_w = self.build_resizer('w'),
+        resizer_e = self.build_resizer('e');
 
     if($(newNode).children('.' + gt + '-resizer').length === 0) {
       $(newNode).prepend(resizer_w).append(resizer_e);
     }
 
     $('.' + gt + '-resizer').mousedown(function() {
-      var self       = $(this),
+      var _self      = $(this),
           tag        = obj[0].children[$(this).parent().index()],
           tag_type   = $(tag).attr("data-" + gt);
 
-      clear_selections();
+      self.clear_selections();
       $(obj)[gt]("destroy");
 
       $(obj).unbind(eventNameResize).bind(eventNameResize, function() {
-        var sel        = get_selections(),
+        var sel        = self.get_selections(),
             range      = sel.getRangeAt(0),
-            intersects = range_intersects(range, tag.childNodes[1]),
+            intersects = self.range_intersects(range, tag.childNodes[1]),
             new_range  = document.createRange(),
             residual   = range.cloneRange(),
             offset     = range.toString().length,
             contents   = "";
 
-        if(range_intersects_tag(range, $(obj), $(tag))) {
-          clear_selections();
-          $(this).unbind(eventNameResize).bind(eventName, { 'settings' : settings }, tag_selected);
+        if(self.range_intersects_tag(range, $(obj), $(tag))) {
+          self.clear_selections();
+          $(this).unbind(eventNameResize).bind(eventName, { 'settings' : settings }, self.tag_selected);
           settings.onOverlapWarning.call();
           return;
         }
 
         try {
-          if(self.hasClass(gt + "-resizer-e")) {
+          if(_self.hasClass(gt + "-resizer-e")) {
             if(intersects) {
               new_range.setStart(tag.childNodes[1], 0);
               new_range.setEnd(tag.childNodes[1], tag.childNodes[1].length-offset);
@@ -173,7 +177,7 @@
               new_range.setStart(tag.childNodes[1], 0);
               new_range.setEnd(tag.nextSibling, offset);
             }
-          } else if(self.hasClass(gt + "-resizer-w")) {
+          } else if(_self.hasClass(gt + "-resizer-w")) {
             if(intersects) {
               new_range.setStart(tag.childNodes[1], offset);
               new_range.setEnd(tag, 2);
@@ -182,35 +186,38 @@
               new_range.setEnd(tag, 2);
             }
           }
-          clear_selections();
+          self.clear_selections();
           sel.addRange(new_range);
           contents = new_range.extractContents().textContent;
-          newNode = $(build_selector(tag_type, contents, $(tag).attr("style")));
-          if(intersects && self.hasClass(gt + "-resizer-e")) { $(tag).after(residual.toString()); }
-          if(intersects && self.hasClass(gt + "-resizer-w")) { $(tag).before(residual.toString()); }
+          newNode = $(self.build_selector(tag_type, contents, $(tag).attr("style")));
+          if(intersects && _self.hasClass(gt + "-resizer-e")) { $(tag).after(residual.toString()); }
+          if(intersects && _self.hasClass(gt + "-resizer-w")) { $(tag).before(residual.toString()); }
           $(tag).before(newNode).remove();
-          add_resizers($(this), settings, newNode);
+          self.add_resizers($(this), settings, newNode);
           obj[0].normalize();
-          settings.onTagged.call(this, $(this), { "tag" : { "type" : tag_type, "value" : contents }, "content" : convert_markup(this) });
-        } catch(e) {
-          clear_selections();
-          $(this).unbind(eventNameResize).bind(eventName, { 'settings' : settings }, tag_selected);
+          settings.onTagged.call(this, $(this), { "tag" : { "type" : tag_type, "value" : contents }, "content" : self.convert_markup(this) });
+        } catch(error) {
+          self.clear_selections();
+          $(this).unbind(eventNameResize).bind(eventName, { 'settings' : settings }, self.tag_selected);
           settings.onOverlapWarning.call();
         }
 
-        clear_selections();
-        $(this).unbind(eventNameResize).bind(eventName, { 'settings' : settings }, tag_selected);
+        self.clear_selections();
+        $(this).unbind(eventNameResize).bind(eventName, { 'settings' : settings }, self.tag_selected);
       });
 
     });
-  },
+  };
 
-  preloader = function(obj, selectors, settings) {
-    var tags = {}, snippet = "";
+  GT.preloader = function(obj, selectors, settings) {
+    var self    = this,
+        tags    = {},
+        snippet = "";
 
     $.each(selectors, function(index0, value0) {
       if($.isArray(value0)) {
         $.each(value0, function(index1, value1) {
+          index1 = null;
           $.each(value1, function(index2, value2) {
             tags[index2] = value2;
           });
@@ -223,64 +230,65 @@
     $.each(tags, function(index, value) {
       snippet = $('[data-' + gt + '=' + index + ']', obj);
       if(snippet.length > 1 && !settings.multitag) {
-        $(snippet[0]).addClass(gt + '-selector ' + gt + '-tag').attr('title', index).attr('style', get_style(value));
-        add_resizers($(obj), settings, $(snippet[0]));
+        $(snippet[0]).addClass(gt + '-selector ' + gt + '-tag').attr('title', index).attr('style', self.get_style(value));
+        self.add_resizers($(obj), settings, $(snippet[0]));
       } else {
         snippet.each(function() {
-          $(this).addClass(gt + '-selector ' + gt + '-tag').attr('title', index).attr('style', get_style(value));
-          add_resizers($(obj), settings, $(this));
+          $(this).addClass(gt + '-selector ' + gt + '-tag').attr('title', index).attr('style', self.get_style(value));
+          self.add_resizers($(obj), settings, $(this));
         });
       }
     });
 
-    settings.onActivate.call(this, obj, { "content" : convert_markup(obj) });
-  },
+    settings.onActivate.call(this, obj, { "content" : self.convert_markup(obj) });
+  };
 
-  tag_selected = function(e) {
-    var sel        = get_selections(),
+  GT.tag_selected = function(e) {
+    var self       = GT,
+        sel        = self.get_selections(),
         range      = sel.getRangeAt(0),
         newNode    = "",
-        intersects = false,
         settings   = e.data.settings,
         selected   = '.' + gt + '-tag[data-' + gt + '=' + settings.active_tag + ']';
 
     if(!settings.multitag && $(selected, $(this)).length === 1) {
-      clear_selections();
+      self.clear_selections();
       settings.onMultitagWarning.call();
       return;
     }
 
     if($.trim(sel) !== "") {
       settings.beforeTagged.call(this, $(this));
-      newNode = $(build_selector(settings.active_tag, settings.active_tag, $(selected).attr("style"), false));
+      newNode = $(self.build_selector(settings.active_tag, settings.active_tag, $(selected).attr("style"), false));
       try {
-        if(range_intersects_tags(range, $(this))) {
-          clear_selections();
+        if(self.range_intersects_tags(range, $(this))) {
+          self.clear_selections();
           settings.onOverlapWarning.call();
           return;
         } else {
-          clear_selections();
+          self.clear_selections();
           range.surroundContents(newNode[0]);
-          add_resizers($(this), settings, newNode);
-          settings.onTagged.call(this, $(this), { "tag" : { "type" : settings.active_tag, "value" : range.toString() }, "content" : convert_markup(this) });
+          self.add_resizers($(this), settings, newNode);
+          settings.onTagged.call(this, $(this), { "tag" : { "type" : settings.active_tag, "value" : range.toString() }, "content" : self.convert_markup(this) });
         }
-      } catch(e) {
-        clear_selections();
+      } catch(error) {
+        self.clear_selections();
         settings.onOverlapWarning.call();
         return;
       }
     }
 
-  },
+  };
 
-//TODO: counts get a little wonky when there are groups
-  build_initializer = function(obj, settings) {
-    var counter   = 0,
+//TODO: counts & default color selections get a little wonky when there are groups
+  GT.build_initializer = function(obj, settings) {
+    var self      = this,
+        counter   = 0,
         content   = "",
         button    = "",
         selectors = {},
         selector  = "",
-        obj       = {},
+        tag_obj   = {},
         selected  = "";
 
     $.each(settings.tags, function(index0, value0) {
@@ -292,25 +300,26 @@
             selectors[index0] = selectors[index0] || [];
             if(typeof value1 === "object") {
               $.each(value1, function(index2, value2) {
-                obj = {};
-                obj[index2] = value2;
-                selectors[index0].push(obj);
+                tag_obj = {};
+                tag_obj[index2] = value2;
+                selectors[index0].push(tag_obj);
               });
             } else {
-              obj = {};
-              obj[value1] = (sample_styles[counter]) ? sample_styles[counter] : sample_styles[14];
-              selectors[index0].push(obj);
+              tag_obj = {};
+              tag_obj[value1] = sample_styles[counter] || sample_styles[14];
+              selectors[index0].push(tag_obj);
             }
           }
           counter += 1;
         });
       } else {
-        selectors[value0] = (sample_styles[counter]) ? sample_styles[counter] : sample_styles[14];
+        selectors[value0] = sample_styles[counter] || sample_styles[14];
         counter += 1;
       }
     });
 
     $.each(selectors, function(index, value) {
+      index = null;
       if($.isArray(value)) {
         content = '<div class="' + gt + '-selectors-buttons"><ul></ul></div>';
         return;
@@ -327,7 +336,7 @@
         $('.' + gt + '-selectors-buttons ul').append(button);
         $.each(value0, function() {
           $.each(this, function(index1, value1) {
-            selector = build_selector(index1, index1, get_style(value1), true);
+            selector = self.build_selector(index1, index1, self.get_style(value1), true);
             $('.' + gt + '-selectors-type.' + index0 + ' ul').append(selector);
           });
         });
@@ -335,7 +344,7 @@
     } else {
       $(settings.config_ele).append('<div class="' + gt + '-selectors-type"><ul></ul></div>');
       $.each(selectors, function(index, value) {
-        selector = build_selector(index, index, get_style(value), true);
+        selector = self.build_selector(index, index, self.get_style(value), true);
         $('.' + gt + '-selectors-type ul', settings.config_ele).append(selector).parent().show();
       });
     }
@@ -354,26 +363,26 @@
     }).end().find('.' + gt + '-selector', '.' + gt + '-selectors-type').each(function() {
         if($(this).text() === settings.active_tag) { $(this).addClass('selected'); }
         $(this).click(function(e) {
-          var self = $(this);
+          var _self = $(this);
           e.preventDefault();
           $('.' + gt + '-selectors-type', settings.config_ele).find('.' + gt + '-selector').removeClass("selected");
-          self.addClass("selected");
+          _self.addClass("selected");
           settings.config_activate = false;
-          settings.active_tag = self.text();
+          settings.active_tag = _self.text();
           $(obj)[gt]("destroy")[gt](settings);
         });
       });
 
     return selectors;
-  },
+  };
 
-  methods = {
+  GT.methods = {
     init : function(f, options) {
       f = null;
       return this.each(function() {
         var self = $(this), settings = $.extend({}, $.fn[gt].defaults, options);
-        self.bind(eventName, { 'settings' : settings }, tag_selected);
-        if(settings.config_activate) { preloader(self, build_initializer(self, settings), settings); }
+        self.bind(eventName, { 'settings' : settings }, GT.tag_selected);
+        if(settings.config_activate) { GT.preloader(self, GT.build_initializer(self, settings), settings); }
       });
     },
     remove_all : function() {
@@ -389,14 +398,14 @@
   };
 
   $.fn[gt] = function(method) {
-    if (methods[method]) {
-      return methods[method].apply(this, Array.prototype.slice.call(arguments, 1));
+    if (GT.methods[method]) {
+      return GT.methods[method].apply(this, Array.prototype.slice.call(arguments, 1));
     } else if (typeof method === "function" || !method) {
-      return methods.init.apply(this, arguments);
+      return GT.methods.init.apply(this, arguments);
     } else if (typeof method === "object") {
-      return methods.init.apply(this, [null, method]);
+      return GT.methods.init.apply(this, [null, method]);
     } else {
-      $.error( 'Method ' +  method + ' does not exist on jQuery ' + gt );
+      $.error('Method ' +  method + ' does not exist on jQuery ' + gt);
     }
   };
 
